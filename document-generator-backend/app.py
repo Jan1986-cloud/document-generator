99793c03b68204d1cfbfe985cab098f2921538b3
Jan1986-cloud <jan@pvmonteur.nl>
2025-06-19 08:37:25 +0200

    chore!: Reset and apply final definitive configuration

--- document-generator-backend/app.py
+++ document-generator-backend/app.py
@@ -1,8 +1,3 @@
 from src.main import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    # Gebruik Gunicorn of een andere WSGI server in productie
-    # Deze run is alleen voor lokaal debuggen
-    app.run(host='0.0.0.0', port=8080)
